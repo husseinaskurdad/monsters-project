38610c9dbec18231a16af1eb925de3e57d7d09d2
Hassan Hussein <husseinaskurdad@gmail.com>
2023-12-16 10:57:37 -0800

    check

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ let App = () => {
 
   return (
     <div className="App">
-    <h1 className='app-title'>Monsters Rolodex</h1>
+
     <SearchBox
       onChangeHandler = {onSearchChange}
       placeholder = 'search the damn monsters'
